559a5969d15c047bd515727ac66c79c9f4732684
Ben Paolillo <bppao04@gmail.com>
2016-06-04 17:44:54 -0400

    Testing that TankPlayerController possesses player tank with logging

--- BattleTank/Source/BattleTank/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -3,6 +3,25 @@
 #include "BattleTank.h"
 #include "TankPlayerController.h"
 
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	UE_LOG(LogTemp, Warning, TEXT("PlayerController Begin Play"));
+
+	ATank* controlledTank = nullptr;
+	controlledTank = GetControlledTank();
+
+	if (controlledTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Controlled Tank: %s"), *controlledTank->GetName());
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Failed to possess tank"));
+	}
+}
+
 ATank* ATankPlayerController::GetControlledTank() const
 {
 	return Cast<ATank>(GetPawn());
