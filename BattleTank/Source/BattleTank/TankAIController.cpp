be906ef0ba9170264606a94a1cd968b6f5f97b0e
Ben Paolillo <bppao04@gmail.com>
2016-06-04 18:27:37 -0400

    Testing if AI can find the player tank with logging

--- BattleTank/Source/BattleTank/TankAIController.cpp
+++ BattleTank/Source/BattleTank/TankAIController.cpp
@@ -7,21 +7,27 @@ void ATankAIController::BeginPlay()
 {
 	Super::BeginPlay();
 
-	ATank* controlledTank = nullptr;
-	controlledTank = GetControlledTank();
+	ATank* playerTank = nullptr;
+	playerTank = GetPlayerTank();
 
-	if (controlledTank)
+	if (playerTank)
 	{
-		UE_LOG(LogTemp, Warning, TEXT("AI Controlled Tank: %s"), *controlledTank->GetName());
+		UE_LOG(LogTemp, Warning, TEXT("AIController found player tank: %s"), *playerTank->GetName());
 	}
 	else
 	{
-		UE_LOG(LogTemp, Warning, TEXT("Failed to possess tank"));
+		UE_LOG(LogTemp, Warning, TEXT("Failed to find player tank"));
 	}
 }
 
 ATank* ATankAIController::GetControlledTank() const
 {
 	return Cast<ATank>(GetPawn());
+}
+
+ATank* ATankAIController::GetPlayerTank() const
+{
+	APawn* playerTank = GetWorld()->GetFirstPlayerController()->GetPawn();
+	return Cast<ATank>(playerTank);
 }
 
