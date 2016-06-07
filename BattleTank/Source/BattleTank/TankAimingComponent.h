a0a5ec11d221f6a04f87def0905078fe03c23e1e
Ben Paolillo <bppao04@gmail.com>
2016-06-06 20:29:33 -0400

    Created TankAimingComponent and moved aiming test code

---
+++ BattleTank/Source/BattleTank/TankAimingComponent.h
@@ -0,0 +1,25 @@
+// Ben Paolillo - Copyright 2016
+
+#pragma once
+
+#include "Components/ActorComponent.h"
+#include "TankAimingComponent.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class BATTLETANK_API UTankAimingComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+	public:	
+		// Sets default values for this component's properties
+		UTankAimingComponent();
+
+		// Called when the game starts
+		virtual void BeginPlay() override;
+	
+		// Called every frame
+		virtual void TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction ) override;
+
+		void AimAt(FVector hitLocation);
+};
