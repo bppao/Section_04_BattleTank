b09a99ddb1532de32ebfe13460d6cff47c8e2bac
Ben Paolillo <bppao04@gmail.com>
2016-08-06 02:22:16 -0400

    Implemented code to apply force to the tank to get it moving

--- BattleTank/Source/BattleTank/TankTrack.cpp
+++ BattleTank/Source/BattleTank/TankTrack.cpp
@@ -9,6 +9,12 @@ void UTankTrack::SetThrottle(float throttle)
 	UE_LOG(LogTemp, Warning, TEXT("%s throttle: %f!"), *name, throttle);
 
 	// TODO Clamp throttle value so the user can't increase the scale and make the tank move faster
+	FVector forceApplied = GetForwardVector() * throttle * TrackMaxDrivingForce;
+	FVector forceLocation = GetComponentLocation();
+
+	// Get the root tank component and add force at the location of the track
+	UPrimitiveComponent* tankRoot = Cast<UPrimitiveComponent>(GetOwner()->GetRootComponent());
+	tankRoot->AddForceAtLocation(forceApplied, forceLocation);
 }
 
 
--- BattleTank/Source/BattleTank/TankTrack.h
+++ BattleTank/Source/BattleTank/TankTrack.h
@@ -16,5 +16,9 @@ class BATTLETANK_API UTankTrack : public UStaticMeshComponent
 	public:
 		// Sets a throttle between -1 and +1
 		UFUNCTION(BlueprintCallable, Category = Input)
-		void SetThrottle(float throttle);	
+		void SetThrottle(float throttle);
+
+		// Max force per track in Newtons
+		UPROPERTY(EditDefaultsOnly, Category = Setup)
+		float TrackMaxDrivingForce = 400000.0f; // Assume 40,000 kg tank and 1g acceleration
 };
