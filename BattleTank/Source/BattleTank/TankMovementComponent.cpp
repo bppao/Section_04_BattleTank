b65cc58cf13f8c0ee899fa4980a1a0da8e980d37
Ben Paolillo <bppao04@gmail.com>
2016-08-06 17:23:31 -0400

    Refactored how the TankMovementComponent is added in blueprint
    Also refactored the input binding to intuitively drive the tank with WASD

--- BattleTank/Source/BattleTank/TankMovementComponent.cpp
+++ BattleTank/Source/BattleTank/TankMovementComponent.cpp
@@ -2,10 +2,21 @@
 
 #include "BattleTank.h"
 #include "TankMovementComponent.h"
+#include "TankTrack.h"
 
+void UTankMovementComponent::Initialize(UTankTrack* leftTrackToSet, UTankTrack* rightTrackToSet)
+{
+	if (!leftTrackToSet || !rightTrackToSet) return;
+
+	m_LeftTrack = leftTrackToSet;
+	m_RightTrack = rightTrackToSet;
+}
 
 void UTankMovementComponent::IntendMoveForward(float input)
 {
 	UE_LOG(LogTemp, Warning, TEXT("Intend move forward input: %f"), input);
+
+	m_LeftTrack->SetThrottle(input);
+	m_RightTrack->SetThrottle(input);
 }
 
